84ad80d89cdeb3ecc735a6cb63a93dbe0af85ac8
Alli <alli.a.makinen@ericsson.com>
2017-03-30 18:14:03 +0300

    add script to launch several devices in one container

---
+++ devices/start_clients.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+max=10
+i=1
+
+# SIGTERM-handler
+sigterm_handler() {
+  killall node
+  wait 
+  exit 143; # 128 + 15 -- SIGTERM
+}
+
+# SIGINT-handler
+sigint_handler() {  
+  killall node
+  wait 
+  exit 130; # 128 + 2 -- SIGINT
+}
+
+trap 'sigint_handler' SIGINT
+trap 'sigterm_handler' SIGTERM
+
+
+while [ $i -le $max ]; do
+  #echo 'Starting client #'$i
+  exec node $1 $2 &
+  i=$(expr $i + 1)
+done
+
+wait
+
+
